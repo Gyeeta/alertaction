5b4eb178e333ec95251b451e01ca7e5da15ba77a
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-08-17 17:55:58 +0530

    More docker changes
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -x
 
-DOCKER_BUILDKIT=1 docker build -t gyeeta/alertaction:latest -t ghcr.io/gyeeta/alertaction:latest -f ./Dockerfile --build-arg ALERTACTION_VERSION="v`./runalertaction.sh --version | grep Version | cut -d " " -f 6`" .
+ALERTACTION_VERSION="`./runalertaction.sh --version | grep Version | cut -d " " -f 6`"
+
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/alertaction:latest -t ghcr.io/gyeeta/alertaction:"$ALERTACTION_VERSION" -f ./Dockerfile --build-arg ALERTACTION_VERSION=v"${ALERTACTION_VERSION}"  .
 
