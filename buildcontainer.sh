81b13cb79e5ddb8321dfdedeb8ff02ccee3b3a07
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-02 13:05:10 +0530

    Force no cache
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- buildcontainer.sh
+++ buildcontainer.sh
@@ -2,5 +2,5 @@
 
 ALERTACTION_VERSION="`./runalertaction.sh --version | grep Version | cut -d " " -f 6`"
 
-DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/alertaction:latest -t ghcr.io/gyeeta/alertaction:"$ALERTACTION_VERSION" -f ./Dockerfile --build-arg ALERTACTION_VERSION=v"${ALERTACTION_VERSION}"  .
+DOCKER_BUILDKIT=1 docker build -t ghcr.io/gyeeta/alertaction:latest -t ghcr.io/gyeeta/alertaction:"$ALERTACTION_VERSION" -f ./Dockerfile --no-cache --build-arg ALERTACTION_VERSION=v"${ALERTACTION_VERSION}"  .
 
